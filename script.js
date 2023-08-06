d97187e908221de83e2e93af4ddc9e67f4328318
BurakOsmanClk <104213644+BurakOsmanClk@users.noreply.github.com>
2023-08-06 23:51:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-
-let i = 0;
-let images = ['url("/images/resim1.jpg")','url("images/resim2.jpg")', 'url("images/resim3.jpg")', 'url("images/resim4.jpg")', 'url("images/resim5.jpg")'];
-function changeBG(){
-  const element = document.getElementById("anabody");
-  if (element.className == "anabody") { 
-    document.body.style.backgroundImage = images[i];
-      i++;
-      if(i>=colors.length){
-          i=0;
-      };
-  }
-      
-}
-setInterval(changeBG, 5000);
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
