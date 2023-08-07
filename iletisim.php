82624d9c150ca1f400325e826f87dba9dc72b723
BurakOsmanClk <104213644+BurakOsmanClk@users.noreply.github.com>
2023-08-07 23:04:40 +0300

    Add files via upload

---
+++ iletisim.php
@@ -0,0 +1,6 @@
+<?php
+echo $_POST['ad'];
+echo $_POST['soyad'];
+echo $_POST['dosya'];
+echo $_POST['mesaj'];
+?>
--- script.js
+++ script.js
@@ -4,3 +4,22 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
